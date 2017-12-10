5df0155f652da423d954c5fb1c39fd1a0a982298
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2017-12-09 22:41:18 -0500

    addec changes

--- src/components/resultpage.jsx
+++ src/components/resultpage.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
 import Header from './header.jsx';
-var info = '';
+
 
 class App extends React.Component {
 
-  getdata()
+  constructor(props)
   {
-  
+    super();
+    this.state={
+      info:[]
+    };
+  }
+  componentWillMount()
+  {
+
   fetch('/foodquery/getdata')
   .then (results =>{
     return results.json()
   }).then (data=> {
 
- info = data;
- console.log(data);
+ this.setState({info : data[0].concat(data[1])});
+
 })
 
 }
 
 
   render () {
-    this.getdata();
-    return (<div><Header/>
-      <Result/>
+    var divStyle = {
+      display:"flex",
+      flexFlow: "row wrap"
+
+
+    }
+    var HeadingStyle={
+      display:"flex",
+      flexFlow: "row wrap",
+      alignItems: "center",
+  justifyContent: "center",
+      marginTop:"5%"
+    }
+
+    var resultlist =this.state.info.map(function(element){
+      return <Result/>;
+    });
+    console.log(this.state.info);
+    return (<div ><Header /><div style = {HeadingStyle}>
+      {resultlist}
+      </div>
     </div>)
   }
 }
@@ -31,10 +56,13 @@ class Result extends React.Component{
 
   render(){
     var BoxStyle = {
-      height:100,
-      width:200,
-      border:"solid 2px",
-      borderColor:"#C4625D"
+      height:150,
+      width:250,
+      padding:0,
+      backgroundColor:"#FFF",
+      webkitFilter: "drop-shadow (0px 0px 5px #666)",
+      filter: "drop-shadow(0px 0px 5px #666)",
+      margin:"2%"
 
     }
   return (<div style = {BoxStyle}>
